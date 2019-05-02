050c4e1e3175509b7226ca8bd7e2bd8694be7ec3
Matthew Wilson <wilsonmatt219@gmail.com>
2019-05-02 22:17:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,12 @@ class Artist
     @@song_count += 1
   end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
 
 
 end
